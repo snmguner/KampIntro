7eaa8c9af37cb3a77f9b0740b424daec1575cc17
Senem Güner <seenem.guner@outlook.com>
2021-01-23 14:26:27 +0300

    16 ocak son ders

--- ClassIntro/Program.cs
+++ ClassIntro/Program.cs
@@ -6,40 +6,52 @@ namespace ClassIntro
     {
         static void Main(string[] args)
         {
-            string adi = "engin ";
+            string adi = "Engin";
             int yas = 36;
 
-            Kurs  kurs1 = new Kurs();
-            kurs1.kursAdi = "Java";
-            kurs1.Egitmen = "Kerem Varış";
-            kurs1.IzlenmeOrani = 64;
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.Egitmen = "Engin Demiroğ";
+            kurs1.IzlenmeOrani = 68;
 
-            Kurs kurs2= new Kurs();
-            kurs2.kursAdi = "c#";
-            kurs2.Egitmen = "Engin Demiroğ";
-            kurs2.IzlenmeOrani = 68;
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Java";
+            kurs2.Egitmen = "Kerem Varış";
+            kurs2.IzlenmeOrani = 64;
 
             Kurs kurs3 = new Kurs();
-            kurs3.kursAdi = "Python";
+            kurs3.KursAdi = "Python";
             kurs3.Egitmen = "Berkay Bilgin";
             kurs3.IzlenmeOrani = 80;
 
-            //Console.WriteLine(kurs1.kursAdi + ":" + kurs1.Egitmen);
+            Kurs kurs4 = new Kurs();
+            kurs4.KursAdi = "C++";
+            kurs4.Egitmen = "Murat Kurtboğan";
+            kurs4.IzlenmeOrani = 100;
 
-            Kurs[] kurslar = new Kurs[] {kurs1,kurs2,kurs3 };
-            foreach (Kurs  kurs in kurslar)
+
+
+
+            //Console.WriteLine(kurs1.KursAdi + " : " + kurs1.Egitmen);
+
+            Kurs[] kurslar = new Kurs[] { kurs1, kurs2, kurs3, kurs4 };
+
+            foreach (var kurs in kurslar)
             {
-                Console.WriteLine(kurs1.kursAdi + ":" + kurs1.Egitmen;
+                Console.WriteLine(kurs.KursAdi + " : " + kurs.Egitmen);
             }
 
-            
-            
+
+
+
+
         }
     }
 
+
     class Kurs
     {
-        public string kursAdi { get; set; }
+        public string KursAdi { get; set; }
         public string Egitmen { get; set; }
         public int IzlenmeOrani { get; set; }
     }
